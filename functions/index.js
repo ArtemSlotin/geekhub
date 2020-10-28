629bc5e25c61f8ce7985f1e7cc7b0dbe3a70b4ae
Artem <orient.ck@gmail.com>
2020-10-28 22:23:09 +0200

    Homework 1 Artem Slotin

--- functions/index.js
+++ functions/index.js
@@ -1,23 +1,30 @@
 document.querySelectorAll('input[name], #formula').forEach(function (input) {
-	input.addEventListener('keyup', function () {
-		var a1 = document.querySelector('[name="a1"]');
-		var b1 = document.querySelector('[name="b1"]');
-		var a2 = document.querySelector('[name="a2"]');
-		var b2 = document.querySelector('[name="b2"]');
-		var formula = document.querySelector('#formula');
-		var condition = document.querySelector('#condition');
-		var result = document.querySelector('#result');
-
-		try {
-			var calculator = new Function('a1, b1, a2, b2', 'return ' + formula.value + ';');
-
-			result.value = calculator(Number(a1.value), Number(b1.value), Number(a2.value), Number(b2.value));
-		}
-		catch (error) {
-			result.value = '#ERROR';
-			console.error(error.message);
-		}
+    input.addEventListener('keyup', function () {
+        var a1 = document.querySelector('[name="a1"]');
+        var b1 = document.querySelector('[name="b1"]');
+        var a2 = document.querySelector('[name="a2"]');
+        var b2 = document.querySelector('[name="b2"]');
+        var formula = document.querySelector('#formula');
+        var condition = document.querySelector('#condition');
+        var result = document.querySelector('#result');
 
 
-	});
+        try {
+            var calculator = new Function('a1, b1, a2, b2', 'return ' + formula.value + ';');
+
+            result.value = calculator(Number(a1.value), Number(b1.value), Number(a2.value), Number(b2.value));
+        } catch (error) {
+            result.value = '#ERROR';
+            console.error(error.message);
+        }
+
+        var validation = new Function('a1, b1, a2, b2', 'return ' + condition.value + ';');
+        var checkCondition = validation(Number(a1.value), Number(b1.value), Number(a2.value), Number(b2.value));
+
+        if (checkCondition) {
+            result.style.backgroundColor = '#B6D7A8'
+        } else (
+            result.style.backgroundColor = '#FFFFFF'
+        )
+    });
 });
