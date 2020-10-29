94101ee9aefd6d355a63f3e9e01f7dcb3b34918b
Artem <orient.ck@gmail.com>
2020-10-29 17:53:44 +0200

    Second try for HomeWork1

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,13 @@
+var a1 = document.querySelector('[name="a1"]');
+var b1 = document.querySelector('[name="b1"]');
+var a2 = document.querySelector('[name="a2"]');
+var b2 = document.querySelector('[name="b2"]');
+var formula = document.querySelector('#formula');
+var condition = document.querySelector('#condition');
+var result = document.querySelector('#result');
+
 document.querySelectorAll('input[name], #formula').forEach(function (input) {
     input.addEventListener('keyup', function () {
-        var a1 = document.querySelector('[name="a1"]');
-        var b1 = document.querySelector('[name="b1"]');
-        var a2 = document.querySelector('[name="a2"]');
-        var b2 = document.querySelector('[name="b2"]');
-        var formula = document.querySelector('#formula');
-        var condition = document.querySelector('#condition');
-        var result = document.querySelector('#result');
-
-
         try {
             var calculator = new Function('a1, b1, a2, b2', 'return ' + formula.value + ';');
 
@@ -17,14 +16,20 @@ document.querySelectorAll('input[name], #formula').forEach(function (input) {
             result.value = '#ERROR';
             console.error(error.message);
         }
+    });
+});
 
+condition.addEventListener('keyup', function () {
+    var result = document.querySelector('#result');
+    try {
         var validation = new Function('a1, b1, a2, b2', 'return ' + condition.value + ';');
         var checkCondition = validation(Number(a1.value), Number(b1.value), Number(a2.value), Number(b2.value));
-
         if (checkCondition) {
             result.style.backgroundColor = '#B6D7A8'
         } else (
             result.style.backgroundColor = '#FFFFFF'
-        )
-    });
+        );
+    } catch (error) {
+        console.log(error);
+    }
 });
